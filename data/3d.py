90e6fd6d4919bd70bb957dffcb9f8a80fbfdee68
Darren Ng <darren.ng@ucdconnect.ie>
2016-03-29 08:24:52 +0100

    epoch

---
+++ data/3d.py
@@ -0,0 +1,27 @@
+__author__ = 'darre_000'
+
+'Self lr 0.001 rmse'
+
+#READ CSV
+lines = [line.rstrip('\n') for line in open('Self lr 0.00001 rmse')]
+sub = []
+avg = []
+av = 0
+for line in lines:
+    sub.append(line.split("\t"))
+
+for x in sub:
+    for y in x:
+      if(float(y) < 1 and float(y) > 0):
+            av+=float(y)
+    avg.append(av/4)
+    av = 0
+##############################################
+
+#Plotting
+import matplotlib.pyplot as plt
+import numpy as n
+
+plt.title("Self lr 0.0001 rmse || average")
+plt.plot(avg)
+plt.show()
--- src/NN/NNSettings.java
+++ src/NN/NNSettings.java
@@ -4,9 +4,9 @@ public class NNSettings {
     public static int inputSize = 216, outputSize = 5, testCount = 0;
     public static String nn = "lib/NN.nnet", 
 	    randNN = "lib/Whist(216i - 60h - 5o).nnet", 
-	    player1GameFile = "data/(Game) Player1 Train 100.txt", 
-	    player2GameFile = "data/(Game) Player2 Train 100.txt",
-	    player1TestFile = "data/(Game) Player1 Test 100.txt",
-	    player2TestFile = "data/(Game) Player2 Test 100.txt";
+	    player1GameFile = "data/(Game) Player1 Train 50.txt", 
+	    player2GameFile = "data/(Game) Player2 Train 50.txt",
+	    player1TestFile = "data/(Game) Player1 Test 50.txt",
+	    player2TestFile = "data/(Game) Player2 Test 50.txt";
     public static double learningRate = 0.0005, lambda = 0.5;
 }
